d5264c8bd1291814d6111c30ee60df4e17801486
Kirill Babich <kilimondgaro@gmail.com>
2024-12-04 21:14:09 +0100

    v0.0.3

--- hinoki-planner.rb
+++ hinoki-planner.rb
@@ -1,10 +1,10 @@
 class HinokiPlanner < Formula
   desc "Command-line planner for efficient goal management"
   homepage "https://github.com/kilimondjaro/hinoki-cli.git"
-  version "0.0.2"
+  version "0.0.3"
 
-  url "https://github.com/kilimondjaro/hinoki-cli/releases/download/v0.0.2/hinoki-planner-darwin-universal.tar.gz"
-  sha256 "65f2ab1baffe6f0329feb6e501e2cc281fbbd4a99c2f48029ce554cc809b8049"
+  url "https://github.com/kilimondjaro/hinoki-cli/releases/download/v0.0.3/hinoki-planner-darwin-universal.tar.gz"
+  sha256 "86f0bed169d9854045d3ef5ed34cca96f9c8cd660b5a8a0a71d4c34dbcfaaa71"
 
   def install
     bin.install "hinoki-planner-darwin-universal" => "hinoki-planner"
